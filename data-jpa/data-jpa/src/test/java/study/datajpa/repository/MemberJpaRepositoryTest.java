99738563b40f34939c4c156d013ec738078c7140
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-22 11:49:31 +0900

    공통 인터페이스 적용

--- data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,10 +1,11 @@
-package study.datajpa.entity;
+package study.datajpa.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberJpaRepository;
 
 import java.util.List;
