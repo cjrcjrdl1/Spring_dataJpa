968cb95f6a8f826ed974a4b8efc20e5aa3b89140
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-23 15:50:30 +0900

    새로운 엔티티를 구별하는 방법

--- data-jpa/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -68,8 +68,8 @@ class MemberTest {
 
         Member findMember = memberRepository.findById(member.getId()).get();
 
-        System.out.println("findMember.getCreatedDate() = " + findMember.getCreatedDate());
-        System.out.println("findMember.getUpdatedDate() = " + findMember.getLastModifiedDate());
+//        System.out.println("findMember.getCreatedDate() = " + findMember.getCreatedDate());
+//        System.out.println("findMember.getUpdatedDate() = " + findMember.getLastModifiedDate());
         System.out.println("findMember.getCreatedBy() = " + findMember.getCreatedBy());
         System.out.println("findMember.getLastModifiedBy() = " + findMember.getLastModifiedBy());
     }
