ed1599ad7b7ab6b54f20d8beb30beab1e0f05783
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-12-23 11:25:41 +0900

    사용자 정의 리포지토리 구현

--- data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -255,5 +255,10 @@ class MemberRepositoryTest {
 
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
 
